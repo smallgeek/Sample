bad5b2a459e3a508e34c10e71405c111dfaa069a
Small Geek <smallgeek@live.jp>
2017-04-30 09:27:25 +0900

    画面を表示できるようにした

---
+++ src/MergePartition/App.fs
@@ -0,0 +1,10 @@
+﻿open System
+open FsXaml
+
+type App = XAML<"App.xaml">
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv = 
+    let app = App();
+    app.Run()
---
+++ src/MergePartition/MainViewModel.fs
@@ -0,0 +1,12 @@
+﻿namespace ViewModels
+
+open System
+open System.Windows
+open System.Windows.Input
+
+open FsXaml
+
+type MainViewModel() = 
+    member x.Title with get() = "Merge & Partition"
+
+
