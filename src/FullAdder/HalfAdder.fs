8145b93201215104a8d036024ff879cc24e27d92
Small Geek <smallgeek@live.jp>
2017-05-03 14:37:15 +0900

    半加算器を使用して全加算器をシミュレートするソースを追加

---
+++ src/FullAdder/HalfAdder.fs
@@ -0,0 +1,19 @@
+﻿module Adder
+  open Unqualified
+  open System
+  open System.Reactive.Subjects
+
+  type HalfAdder() =
+    let a = new BehaviorSubject<int>(0)
+    let b = new BehaviorSubject<int>(0)
+
+    let orGate  = zip a b |> map (fun (x, y) -> x ||| y)
+    let andGate = zip a b |> map (fun (x, y) -> x &&& y)
+    let notGate = andGate |> map (fun x -> ~~~ x)
+    let s = zip orGate notGate |> map (fun (x, y) -> x &&& y)
+    let c = andGate
+
+    member this.InputA = a.OnNext
+    member this.InputB = b.OnNext
+    member val Sum = s
+    member val Carry = c
---
+++ src/FullAdder/Program.fs
@@ -0,0 +1,36 @@
+﻿open Adder
+  open Unqualified
+  open System.Reactive.Subjects
+
+[<EntryPoint>]
+let main argv = 
+  
+  let a = new BehaviorSubject<int>(0)
+  let b = new BehaviorSubject<int>(0)
+  let x = new BehaviorSubject<int>(0)
+
+  let upperHalfAdder = HalfAdder()
+  let lowerHalfAdder = HalfAdder()
+
+  a |> subscribe upperHalfAdder.InputA |> ignore
+  b |> subscribe upperHalfAdder.InputB |> ignore
+  x |> subscribe lowerHalfAdder.InputB |> ignore
+  upperHalfAdder.Sum |> subscribe lowerHalfAdder.InputA |> ignore
+
+  let s = lowerHalfAdder.Sum
+  let c = zip upperHalfAdder.Carry lowerHalfAdder.Carry |> map (fun (x, y) -> x ||| y)
+
+  c |> subscribe (fun x -> x |> printf "C %i ") |> ignore
+  s |> subscribe (fun x -> x |> printfn "S %i") |> ignore
+
+  // Truth table
+  //a.OnNext 0; b.OnNext 0; x.OnNext 0
+  a.OnNext 0; b.OnNext 0; x.OnNext 1
+  a.OnNext 0; b.OnNext 1; x.OnNext 0
+  a.OnNext 0; b.OnNext 1; x.OnNext 1
+  a.OnNext 1; b.OnNext 0; x.OnNext 0
+  a.OnNext 1; b.OnNext 0; x.OnNext 1
+  a.OnNext 1; b.OnNext 1; x.OnNext 0
+  a.OnNext 1; b.OnNext 1; x.OnNext 1
+
+  0
