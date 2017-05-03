efab0201ebc06ef401d4c3249762fa46e9421d66
Small Geek <smallgeek@live.jp>
2017-05-03 10:01:32 +0900

    半加算器をシミュレートするソースを追加

---
+++ src/HalfAdder/Program.fs
@@ -0,0 +1,25 @@
+﻿open Unqualified
+open System.Reactive.Subjects
+
+[<EntryPoint>]
+let main argv = 
+
+  let a = new BehaviorSubject<int>(0)
+  let b = new BehaviorSubject<int>(0)
+
+  let orGate  = zip a b |> map (fun (x, y) -> x ||| y)
+  let andGate = zip a b |> map (fun (x, y) -> x &&& y)
+  let notGate = andGate |> map (fun x -> ~~~ x)
+  let s = zip orGate notGate |> map (fun (x, y) -> x &&& y)
+  let c = andGate
+
+  c |> subscribe (fun x -> x |> printf "C %i ") |> ignore
+  s |> subscribe (fun x -> x |> printfn "S %i") |> ignore
+
+  // Truth table
+  //a.OnNext(false); b.OnNext(false)
+  a.OnNext 0; b.OnNext 1
+  a.OnNext 1; b.OnNext 0
+  a.OnNext 1; b.OnNext 1
+
+  0
---
+++ src/HalfAdder/Unqualified.fs
@@ -0,0 +1,8 @@
+﻿module Unqualified
+  open System
+  open FSharp.Control.Reactive
+
+  let zip = Observable.zip
+  let map = Observable.map
+  let subscribe = Observable.subscribe
+
